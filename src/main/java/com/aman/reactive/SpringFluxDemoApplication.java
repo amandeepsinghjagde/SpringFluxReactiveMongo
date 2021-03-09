7619f4802096e804b3303161469374b2c8ceef92
amandeep <amandeeps@drivewithdarby.com>
2021-03-09 16:34:16 +0530

    Initial Commit of all files

---
+++ src/main/java/com/aman/reactive/SpringFluxDemoApplication.java
@@ -0,0 +1,13 @@
+package com.aman.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFluxDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFluxDemoApplication.class, args);
+	}
+
+}
